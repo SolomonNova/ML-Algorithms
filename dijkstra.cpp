61d2338868fb34f90a4ec3c0a0c86854cf7c71ab
SolomonNova <993284vks@gmail.com>
2026-04-03 08:38:46 +0530

    dijkistra's completed

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -5,7 +5,6 @@
 #include <climits>
 
 using namespace std;
-
 template <typename vertexName = char, typename weight = int>
 
 class graph
@@ -20,6 +19,7 @@ class graph
         {
             vertices[v].push_back(p);
             vertices[p.first];
+            cout << v << "----" << p.second << "----" << p.first << "\n\n";
         }
 
         /////////////////////////////////////////////////////////////////////
@@ -51,30 +51,81 @@ class graph
         /////////////////////////////////////////////////////////////////////
         void dijkstra(vertexName v)
         {
-            // priority queue to be implemente
-            vector<int> distances(vertices.size());
-            vector<char> previous(vertices.size());
+            unordered_map<vertexName, weight> distances;
+            unordered_map<vertexName, vertexName> parents;
 
-            for (auto &distance : distances)
-                distance = INT_MAX;
-
-            for (auto &prev : previous)
-                prev = '\0';
-
-            distance[v] = 0;
+            for (auto &vertex : vertices)
+            {
+                distances[vertex.first] = INT_MAX;
+                parents[vertex.first] = '\0';
+            }
+           
+            priority_queue
+            <
+                pair<weight, vertexName>,
+                vector<pair<weight, vertexName>>,
+                greater<pair<weight, vertexName>>
+            > minHeap;
             
-        }
+            distances[v] = 0;
+            minHeap.push({0, v});
 
+            while (!minHeap.empty())
+            {
+                auto [dist, node] = minHeap.top();
+                minHeap.pop();
+
+                if (dist != distances[node])
+                    continue;
+                
+                for (auto &neighbor : vertices[node])
+                {
+                    vertexName adj = neighbor.first;
+                    weight w = neighbor.second;
+
+                    if (distances[adj] > w + dist)
+                    {
+                        distances[adj] = w + dist;
+                        parents[adj] = node;
+                        minHeap.push({ distances[adj], adj});
+                    }
+                }
+            }
+                
+            // printing the parents and ditanecs finally
+            for (auto &distance : distances) 
+                cout << "distance: " << distance.first << " weight: " << distance.second << "\n";
+
+            cout << "\n";
+            
+            for (auto &parent : parents)
+                cout << "parent of: " << parent.first << " is: " << parent.second << "\n";
+        }
 };
 
 int main()
 {
-    graph G;
-    G.add_edge('A', {'B', 5});
-    G.add_edge('B', {'A', 5});
-    G.print_graph();
+    graph<> G;
 
+    // A connections
+    G.add_edge('A', {'B', 4});
+    G.add_edge('A', {'C', 2});
+ 
+    // B connections
+    G.add_edge('B', {'A', 4});
+    G.add_edge('B', {'C', 1});
+    G.add_edge('B', {'D', 5});
 
+    // C connections
+    G.add_edge('C', {'A', 2});
+    G.add_edge('C', {'B', 1});
+
+    // D connections
+    G.add_edge('D', {'B', 2});
+
+    G.dijkstra('A');
+    // G.print_graph();
+    
     return 0;
 }
 
